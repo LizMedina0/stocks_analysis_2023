fcd3d972b4e7b1b12eb81fe44478103f7e4574ea
LizMedina0 <elisabethmedina0637@yahoo.com>
2023-06-25 11:13:39 +0200

    separated dataframes

--- data_extraction.py
+++ data_extraction.py
@@ -1,6 +1,18 @@
-import numpy as np
 import pandas as pd
 import yfinance
 
 sp_wiki_url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
-sp_list = pd.read_html(sp_wiki_url)
+sp_wiki_list = pd.read_html(sp_wiki_url)
+
+sp_dataframe = sp_wiki_list[0]
+company_list = sp_dataframe["Symbol"].values.tolist()
+
+df = yfinance.download(company_list)
+
+adj_close_df = df["Adj Close"]
+close_df = df["Close"]
+high_df = df["High"]
+low_df = df["Low"]
+open_df = df["Open"]
+volume_df = df["Volume"]
+range_df = high_df - low_df
